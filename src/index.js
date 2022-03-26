491586fda4ef7f6e44feb29d5b3f6d90f5ee8143
amDeq <amyadav@deqode.com>
2022-03-26 21:28:03 +0530

    react learning about how to map data

--- src/index.js
+++ src/index.js
@@ -2,50 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 //CSS
 import "./index.css";
-const firtsBook = {
-  image: "https://m.media-amazon.com/images/I/715KxZLSFBL._AC_UY218_.jpg",
-  title: "Brain Games for Clever Games, 1",
-  author: "Gareth Moore",
-};
-const secondBook = {
-  image: "https://m.media-amazon.com/images/I/715KxZLSFBL._AC_UY218_.jpg",
-  title: "Brain Games for Clever Games, 1",
-  author: "Gareth Moore",
-};
-// const title = "Brain Games for Clever Games, 1";
-// const author = "Gareth Moore";
-// const image = "https://m.media-amazon.com/images/I/715KxZLSFBL._AC_UY218_.jpg";
+const books = [
+  {
+    image: "https://m.media-amazon.com/images/I/715KxZLSFBL._AC_UY218_.jpg",
+    title: "Brain Games for Clever Games, 1",
+    author: "Gareth Moore",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/715KxZLSFBL._AC_UY218_.jpg",
+    title: "Brain Games for Clever Games, 1",
+    author: "Gareth Moore",
+  },
+];
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        image={firtsBook.image}
-        title={firtsBook.title}
-        author={firtsBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta
-          leo eu laoreet ultricies. Phasellus imperdiet risus ac nibh fermentum,
-          sed lobortis lectus aliquam.{" "}
-        </p>
-      </Book>
-      <Book
-        image={secondBook.image}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        const { image, title, author } = book;
+        return <Book image={image} title={title} author={author} />;
+      })}
     </section>
   );
 }
 
-const Book = ({ image, title, author, children }) => {
-  // const { image, title, author } = props;
-  // console.log(props);
+const Book = ({ image, title, author }) => {
   return (
     <article className="book">
       <img src={image} alt="book" />
       <h1>{title}</h1>
-      {children}
+
       <h2>{author}</h2>
     </article>
   );
